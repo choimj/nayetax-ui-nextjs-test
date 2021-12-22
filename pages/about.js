da6bab418cce054946ff4b53bd1fd425b045c6eb
sadb0101 <sadb0101@naver.com>
2021-12-22 16:07:46 +0900

    로그

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,23 @@
 import Link from 'next/link';
 
-const AboutPage = () => (
-  <>
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <input type="file" />
-    <img src="https://api0.wehago.com/SynapDocViewServer/thumbnail/065f768b3e904bdfa456fa61171484e3/0" />
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-  </>
-);
+const AboutPage = () => {
+  const onChange = e => {
+    window.ReactNativeWebView.postMessage(JSON.stringify(e));
+    // console.log(e);
+  };
+  return (
+    <>
+      <h1>About</h1>
+      <p>This is the about page</p>
+      <input type="file" onChange={onChange} />
+      <img src="https://api0.wehago.com/SynapDocViewServer/thumbnail/065f768b3e904bdfa456fa61171484e3/0" />
+      <p>
+        <Link href="/">
+          <a>Go home</a>
+        </Link>
+      </p>
+    </>
+  );
+};
 
 export default AboutPage;
