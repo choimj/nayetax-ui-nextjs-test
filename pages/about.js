9c812d50dd167307414cd76e74c8194001fd3c6a
sadb0101 <sadb0101@naver.com>
2021-12-22 15:53:11 +0900

    이미지

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const AboutPage = () => (
   <>
     <h1>About</h1>
     <p>This is the about page</p>
     <input type="file" />
+    <img src="https://api0.wehago.com/SynapDocViewServer/thumbnail/065f768b3e904bdfa456fa61171484e3/0" />
     <p>
       <Link href="/">
         <a>Go home</a>
